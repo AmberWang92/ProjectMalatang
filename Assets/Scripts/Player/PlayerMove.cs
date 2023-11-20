0fa5edfb983a266a44137931e7eed243fc778769
Yue Ding <ae8273@student.jamk.fi>
2023-11-21 01:59:01 +0200

    Simple Movement

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float runSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Translate(Vector3.forward * Time.deltaTime * runSpeed,Space.World); 
+
+    }
+}
