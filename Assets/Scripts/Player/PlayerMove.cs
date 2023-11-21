986d9c163c4eda4326b32b9468cc965d9161e459
Yue Ding <ae8273@student.jamk.fi>
2023-11-21 02:24:32 +0200

    Move left and right

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float runSpeed = 3f;
+    public float runSpeed = 3.0f;
+    public float leftrightSpeed = 4.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,21 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       transform.Translate(Vector3.forward * Time.deltaTime * runSpeed,Space.World); 
+       transform.Translate(Vector3.forward * Time.deltaTime * runSpeed,Space.World);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.gameObject.transform.position.x > LevelBounary.leftSide)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * leftrightSpeed);
+            }
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.gameObject.transform.position.x < LevelBounary.rightSide)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * leftrightSpeed * -1);
+            }
+        }
 
     }
 }
