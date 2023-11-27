eba25892579fa3812a4616f465bfbd7ef5004aa1
Yue Ding <ae8273@student.jamk.fi>
2023-11-27 16:21:17 +0200

    Obstacle Improvement

--- Assets/Scripts/Environment/ObstacleCollision.cs
+++ Assets/Scripts/Environment/ObstacleCollision.cs
@@ -6,11 +6,15 @@ public class ObstacleCollision : MonoBehaviour
 {
     public GameObject thePlayer;
     public GameObject charMode1;
+    public AudioSource crashSound;
+    public GameObject mainCam;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerMove>().enabled = false;
         charMode1.GetComponent<Animator>().Play("Stumble Backwards");
+        crashSound.Play();
+        mainCam.GetComponent<Animator>().enabled = true;
     }
 }
