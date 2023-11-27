cae643704a61dacb7c50de2b57c532c030b61df3
Yue Ding <ae8273@student.jamk.fi>
2023-11-27 19:28:25 +0200

    Jump

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -2,39 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMove : MonoBehaviour
 {
     public float runSpeed = 5.0f;
     public float leftrightSpeed = 4.0f;
     static public bool canMove = false;
+    public GameObject playerObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-       transform.Translate(Vector3.forward * Time.deltaTime * runSpeed,Space.World);
+        transform.Translate(Vector3.forward * Time.deltaTime * runSpeed, Space.World);
         if (canMove == true)
-        { 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            if (this.gameObject.transform.position.x > LevelBounary.leftSide)
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
-                transform.Translate(Vector3.left * Time.deltaTime * leftrightSpeed);
+                if (this.gameObject.transform.position.x > LevelBounary.leftSide)
+                {
+                    transform.Translate(Vector3.left * Time.deltaTime * leftrightSpeed);
+                }
             }
-        }
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
                 if (this.gameObject.transform.position.x < LevelBounary.rightSide)
                 {
                     transform.Translate(Vector3.left * Time.deltaTime * leftrightSpeed * -1);
                 }
+            }
+            }
         }
-        }
-
-    }
+        
 }
+
