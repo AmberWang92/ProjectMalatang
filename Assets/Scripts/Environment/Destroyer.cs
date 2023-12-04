4bb3428f909343fe8590f2710bf0ec960af40a73
Yue Ding <ae8273@student.jamk.fi>
2023-12-04 13:58:19 +0200

    Destroy section

---
+++ Assets/Scripts/Environment/Destroyer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public string parentName;
+    void Start()
+    {
+        parentName = transform.name;
+        StartCoroutine(DestroyClone());
+    }
+    IEnumerator DestroyClone()
+    {
+        yield return new WaitForSeconds(50);
+        if (parentName == "Section(Clone)")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
